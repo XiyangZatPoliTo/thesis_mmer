dbb56437e0d0676f41bdcae2b3e60af5f0cae975
zuxiyang <xiyang.zu.chn@gmail.com>
2024-07-01 21:54:24 +0200

    first commit

---
+++ source/dataloader.py
@@ -0,0 +1,110 @@
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split, StratifiedKFold
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import glob
+import os
+from utils.audio_tools import gvad
+
+
+def get_meta(dataset_dir):
+    """
+    获取原始音频集，并将文件名中的features进行分割，最后返回一个DataFrame，内容为数据集中每个文件的features
+    """
+    # 使用glob查找指定路径下所有子目录内的所有.wav，并返回每一个wav的路径
+    speech_list = glob.glob(os.path.join(dataset_dir, '*/*.wav'))
+    # 定义.wav文件名中的features
+    features = ['modality', 'vocal_channel', 'emotion', 'emotion_intensity', 'statement', 'repetition', 'actor']
+    # 创建一个空的dataframe，列名为features
+    meta_info = pd.DataFrame(columns=features)
+    for fpath in speech_list:
+        # 分离路径名，并保留.wav的filename，without file extension
+        fn = fpath.split("/")[-1].split(".")[0]
+        # 创建一个字典，将文件名中的各部分与features对应起来
+        speech_info = {f: [c] for c, f in zip(fn.split("-"), features)}
+        # 将当前的元数据存进meta_info
+        meta_info = pd.concat((meta_info, pd.DataFrame(speech_info)), axis=0)
+    # 在DataFrame的第一列插入'speech_list'列，内容为所有文件路径
+    meta_info.insert(0, "speech_list", speech_list)
+    return meta_info
+
+
+def split_meta(meta, kfcv=False, n_splits=5, test_ratio=0.2, stratify=True, target=None, verbose=True,
+               random_state=None):
+    """
+    拆分数据集，默认使用StratifiedKFold,StratifiedKFold 是一种用于交叉验证的技术，特别适用于分类问题。
+    它在分割数据集时，确保每个折中各类别的比例与原始数据集中相同。
+    这对于不平衡数据集特别重要，因为它可以保证每个类别在每个折中的分布相似，从而避免因数据不平衡带来的偏差。
+    """
+    if meta.empty:
+        raise ValueError("The input DataFrame 'meta' is empty. Please provide a non-empty DataFrame.")
+
+    if target not in meta.columns:
+        raise ValueError(f"The target column '{target}' does not exist in the DataFrame.")
+
+    if kfcv:
+        # 使用默认的StratifiedKFold对数据集进行拆分
+        skfold = StratifiedKFold(n_splits=n_splits, random_state=random_state, shuffle=True)
+        if verbose:
+            # 输出数据集大小和比例信息
+            N = len(meta)
+            print('size: {}, {}'.format(int(N * (1 - 1 / n_splits)), int(N * 1 / n_splits)))
+            print('ratio: {}, {}'.format(1 - 1 / n_splits, 1 / n_splits))
+        # 返回K折交叉验证的分割结果
+        return skfold.split(meta[['speech_list', target]], meta[target])
+    else:
+        # 否则，进行常规的训练/测试分割
+        if len(meta) < 2:
+            raise ValueError("The input DataFrame 'meta' does not have enough samples to perform a train/test split.")
+
+        train_meta, test_meta = train_test_split(meta[['speech_list', target]],
+                                                 stratify=meta[target] if stratify else None,
+                                                 shuffle=True,
+                                                 test_size=test_ratio,
+                                                 random_state=random_state)
+        if verbose:
+            # 输出数据集大小和比例信息
+            N = len(meta)
+            print('size: {}, {}'.format(len(train_meta), len(test_meta)))
+            print('ratio: {}, {}'.format(len(train_meta) / N, len(test_meta) / N))
+        return train_meta, test_meta
+
+
+def mels2batch(batch):
+    mel_specs = []  # 存储每个信号的梅尔频谱图
+    info_list = []  # 存储每个信号的信息
+    l_list = []  # 存储每个梅尔频谱图的长度
+    for mel_spec, sr, info, fn in batch:
+        info_list.append(info)
+        l_list.append(mel_spec.shape[1])
+        mel_specs.append(mel_spec.transpose(0, 1).contiguous())
+    padded_mel_specs = nn.utils.rnn.pad_sequence(mel_specs, batch_first=True, padding_value=0).unsqueeze(1).transpose(2,
+                                                                                                                      3).contiguous()
+    # 返回填充后的Mel频谱图，情感标签，和每个样本的长度
+    return padded_mel_specs, torch.Tensor(
+        pd.DataFrame(info_list).emotion.values.astype('int16') - 1).long(), torch.tensor(l_list)
+
+
+def sig2batch(batch, mel_specs_kwargs=None):
+    if mel_specs_kwargs is None:
+        mel_specs_kwargs = {}
+    mel_specs = []
+    info_list = []
+    l_list = []
+    for sig, sr, info, fn in batch:
+        info_list.append(info)
+        mel_spec = librosa.power_to_db(librosa.feature.melspectrogram(sig, sr=sr, **mel_specs_kwargs))
+        va_point = gvad(librosa.db_to_amplitude(mel_spec) ** 2)
+        mel_spec = mel_spec[:, va_point[0]:va_point[1]]
+        l_list.append(mel_spec.shape[1])
+        mel_specs.append(torch.Tensor(mel_spec).transpose(0, 1))
+    padded_mel_specs = nn.utils.rnn.pad_sequence(mel_specs, batch_first=True).unsqueeze(1).transpose(2, 3)
+    return padded_mel_specs, torch.Tensor(pd.DataFrame(info_list).emotion.values.astype('int16') - 1).long(), l_list
+
+
+if __name__ == "__main__":
+    # 需要在env.py里同时将数据集地址换掉
+    meta = get_meta('F:/RAVDESS_original/Audio_Speech_Actors_01-24')
---
+++ source/train.py
@@ -0,0 +1,144 @@
+#########################################################################
+## Train AudioEmoRec for Speech Emotion Recognition on RAVDESS dataset  ##
+#########################################################################
+# 若使用conda命令安装依赖包，请用管理员方式打开cmd进行安装，可以避免channel错误
+# 请安装低于numpy==2.0的版本，否则会出现依赖项无法加载的情况
+import torch
+from torch.utils.data import DataLoader
+from trainer import train, evaluate
+from adabelief_pytorch import AdaBelief
+from utils.env import create_config, save_model, create_folder
+from utils.criterion import LabelSmoothingLoss
+from model import AudioEmoRec
+from dataset import RAVDESSMelS
+from dataloader import get_meta, split_meta, mels2batch
+import pickle
+import matplotlib.pyplot as plt
+import os
+import warnings
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+config = create_config()
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  # I tested on the CPU
+print("here is the checkpoint 1")
+print("train model on {}".format(device))
+
+# Extract meta information from .wav file name
+meta = get_meta(config.dataset_dir)
+print(meta.head(3))
+print("here is the checkpoint 2")
+# Mel spectrogram parameters
+mel_kwargs = {"n_mels": 64, "fmax": 6000, "win_length": 2048, "hop_length": 2048 // 3 * 2, "n_fft": 2048 * 2}
+
+if config.kfcv:
+    # Stratified 5-Fold CV
+    splitted_meta = split_meta(meta=meta, kfcv=True, n_splits=5, target='emotion', random_state=config.random_state)
+else:
+    # Hold-out 8:2
+    splitted_meta = split_meta(meta=meta, kfcv=False, test_ratio=0.2, stratify=True, target='emotion',
+                               random_state=config.random_state)
+
+print("here is the checkpoint 3")
+print('OUTPUT_PATH: {}'.format(config.out_dir))
+
+for k, (tr, ts) in enumerate(splitted_meta):
+    if config.kfcv:
+        # K-Fold CV
+        print(f'----------- Fold {k + 1} -----------')
+        train_meta = meta.iloc[tr, :]
+        test_meta = meta.iloc[ts, :]
+    else:
+        # Hold-out
+        train_meta, test_meta = splitted_meta
+    train_dataset = RAVDESSMelS(dataset_dir=config.dataset_dir, mel_specs_kwargs=mel_kwargs,
+                                speech_list=train_meta.speech_list.tolist())
+    test_dataset = RAVDESSMelS(dataset_dir=config.dataset_dir, mel_specs_kwargs=mel_kwargs,
+                               speech_list=test_meta.speech_list.tolist())
+
+    # Train/test dataloader
+    train_dataloader = DataLoader(train_dataset, batch_size=config.batch_size, collate_fn=mels2batch, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=64, collate_fn=mels2batch, shuffle=False)
+
+    # Define model
+    model = AudioEmoRec(input_dim=mel_kwargs['n_mels'], hidden_dim=128, kernel_size=3, num_classes=8).to(device)
+    loss = LabelSmoothingLoss(n_classes=8, smoothing=.1, dim=-1)
+    optimizer = AdaBelief(model.parameters(), lr=1e-3, eps=1e-8, betas=(0.9, 0.999), weight_decay=1e-7,
+                          weight_decouple=False, rectify=False, print_change_log=False)
+    # 学习率调度器，通过监控模型的性能指标来动态调整学习率，帮助模型更好地收敛。
+    # UserWarning: The verbose parameter is deprecated. Please use get_last_lr() to access the learning rate.
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=.7, patience=2, cooldown=3, verbose=True,
+                                                           min_lr=1e-6, mode='min', threshold=0.001)
+
+    MODEL_SAVE_DIR = os.path.join(config.out_dir, f'model/fold{k + 1}/' if config.kfcv else 'model/')
+    LOG_OUT_DIR = os.path.join(config.out_dir, 'logs/')
+    LOG_FILENAME = os.path.join(LOG_OUT_DIR, f'fold{k + 1}_{config.log}' if config.kfcv else config.log)
+    N_EPOCHS = config.n_epochs
+    create_folder(config.out_dir)
+    create_folder(MODEL_SAVE_DIR)
+    create_folder(LOG_OUT_DIR)
+    print('LOG_OUT_PATH: {}'.format(LOG_FILENAME))
+    print('MODEL_OUT_PATH: {}'.format(os.path.join(MODEL_SAVE_DIR, config.model_name)))
+
+    # Training
+    print('Training {} Epochs'.format(N_EPOCHS))
+    train_loss_list = []
+    test_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+
+    best_test_loss = torch.inf  # initialized the best loss to infinity
+    best_test_acc = 0  # initialized the best accuracy to 0
+
+    for epoch in range(N_EPOCHS):
+        train_loss, train_acc = train(train_dataloader, model, optimizer, loss, epoch + 1, device=device)
+        test_loss, test_acc = evaluate(test_dataloader, model, loss, epoch + 1, device=device)
+
+        f = open(f"{LOG_FILENAME}", "a")
+        epoch_result = ("[Epoch {e}] train Loss: {trainL:.5f} Accuracy: {trainAcc:.4f} | test Loss: {testL:.5f} "
+                        "Accuracy: {testAcc:.4f} ").format(
+            e=epoch + 1, trainL=train_loss, trainAcc=train_acc, testL=test_loss, testAcc=test_acc)
+        print(epoch_result, file=f)
+        print(epoch_result)
+
+        scheduler.step(train_loss)
+
+        train_loss_list.append(train_loss)
+        test_loss_list.append(test_loss)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        f.close()
+
+        if test_acc > best_test_acc:
+            save_model(MODEL_SAVE_DIR, test_loss=test_loss, test_acc=test_acc, model=model,
+                       model_name=config.model_name)
+            best_test_acc = test_acc
+            best_test_loss = test_loss
+
+        elif ((best_test_acc - test_acc) < 1e-3) & (test_loss < best_test_loss):
+            save_model(MODEL_SAVE_DIR, test_loss=test_loss, test_acc=test_acc, model=model,
+                       model_name=config.model_name)
+            best_test_acc = test_acc
+            best_test_loss = test_loss
+        else:
+            pass
+
+    # Save training log
+    metric_log_dict = {
+        'train': {
+            'acc': train_acc_list,
+            'loss': train_loss_list
+        },
+        'test': {
+            'acc': test_acc_list,
+            'loss': test_loss_list
+        }
+    }
+
+    with open(os.path.join(LOG_OUT_DIR, (f'fold{k + 1}_' if config.kfcv else '') + 'metric_log_dict.pkl'), 'wb') as f:
+        pickle.dump(metric_log_dict, f)
+
+    # Last epoch, save the model
+    save_model(MODEL_SAVE_DIR, test_loss=test_loss, test_acc=test_acc, model=model, model_name="last_epoch", init=False)
+    if not config.kfcv:
+        break
